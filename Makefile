de9ecfe9e9f2086510b7f7fd2b739ec75f371125
Michał Ciesielski <majalcmaj@gmail.com>
2025-11-23 23:31:17 +0100

    Setup the RAG project

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
+run:
+	cd src && poetry run python main.py
+
 lint:
 	poetry run black src/
 
 test:
 	poetry run pytest src/
+
--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
+import ollama
+
+EMBEDDING_MODEL = "hf.co/CompendiumLabs/bge-base-en-v1.5-gguf"
+LANGUAGE_MODEL = "hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF"
+
+
+def read_cats_data() -> list[str]:
+    with open("../data/cats.txt", "r") as file:
+        cats = file.readlines()
+        print(f"Read {len(cats)} cat facts from cats.txt")
+        return cats
+
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    cats_data = read_cats_data()
